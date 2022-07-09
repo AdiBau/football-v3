7c00c7ad67898126e247bd1e7bf5d2bb706f5368
AdiBau <biuro@adibau.pl>
2022-07-09 21:15:10 +0200

    first commit

--- controllers/statistic.js
+++ controllers/statistic.js
@@ -2,62 +2,73 @@ const Team = require('../models/teams')
 const Player = require('../models/players');
 
 const getStatisticAll = async (req, res) => {
-  const Teams = await Team.find({});
-  let statistic = [];
- Teams.map(elementTeams => {
-    statistic.push({
-      "TeamName": elementTeams.TeamName,
-      "TeamCode": elementTeams.TeamCode,
-      "Nationality": elementTeams.Nationality,
-      "Players": []
-    });
-    const AllPlayers = async ()=>{
-    const Players = await Player.find({ "TeamCode": elementTeams.TeamCode });
-  
-    if (Players) {
-      Players.map(elementPlayers => {
-       statistic[Teams.indexOf(elementTeams)].Players.push(elementPlayers )
-      });
-    }
-    if(Teams.length == (Teams.indexOf(elementTeams)+1))
-    {
-     return res.status(201).json(statistic)
-    }
-  }
-  AllPlayers();
-  });
-  if(!Teams){
-    res.status(404).json({"msg": "No registered clubs"})
-  }
+  try {
+          const Teams = await Team.find({});
+          let statistic = [];
+          Teams.map(elementTeams => {
+            statistic.push({
+              "TeamName": elementTeams.TeamName,
+              "TeamCode": elementTeams.TeamCode,
+              "Nationality": elementTeams.Nationality,
+              "Players": []
+               });
+          const AllPlayers = async ()=>{
+          const Players = await Player.find({ "TeamCode": elementTeams.TeamCode });
+          
+          if (Players) {
+            Players.map(elementPlayers => {
+             statistic[Teams.indexOf(elementTeams)].Players.push(elementPlayers )
+            });
+          }
+          if(Teams.length == (Teams.indexOf(elementTeams)+1))
+          {
+           return res.status(201).json(statistic)
+          }
+        }
+        AllPlayers();
+        });
+        if(!Teams){
+          res.status(404).json({"msg": "No registered clubs"})
+        }
+} catch (error) {
+  res.status(404).json({"msg": error})
+}
 }
 
 const getStatisticClub = async (req, res)=>{
-  let statistic = [];
-  const Teams = await Team.findOne({"TeamCode": req.params.code});
-  const Players = await Player.find({ "TeamCode": req.params.code });
-
-   statistic.push({
-      "TeamName": Teams.TeamName,
-      "TeamCode":Teams.TeamCode,
-      "Nationality": Teams.Nationality,
-      "Players": []
-    });
- 
-    const AllPlayers = async ()=>{
-    if (Players) {
-     await Players.map(elementPlayers => {
-      statistic[0].Players.push(elementPlayers )
-      });
-    }
-   
-   
+  try {
+    const code = req.params.code;
+    console.log(code);
+    
+         let statistic = [];
+        const Teams = await Team.findOne({"TeamCode": req.params.code});
+        const Players = await Player.find({ "TeamCode": req.params.code });
+        if(Teams != null){
+          console.log(Teams);
+          
+         statistic.push({
+            "TeamName": Teams.TeamName,
+            "TeamCode":Teams.TeamCode,
+            "Nationality": Teams.Nationality,
+            "Players": []
+          });
+        
+          const AllPlayers = async ()=>{
+          if (Players) {
+           await Players.map(elementPlayers => {
+            statistic[0].Players.push(elementPlayers )
+            });
+          }
      return res.status(201).json(statistic)
+       }
+       AllPlayers();
+      }else{
+         res.status(404).json({"msg": "No registered club", team: TeamsAll.TeamCode})
+      }
+} catch (error) {
   
-  }
-  AllPlayers();
+  res.status(404).json({"msg": "No registered club","error": error})
   
-  if(!Teams){
-    res.status(404).json({"msg": "No registered club"})
 }
 }
 
